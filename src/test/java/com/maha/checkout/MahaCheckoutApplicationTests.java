fdb6d89f5d0a56f203aa4404daf54b2eca881b45
Dilip Pulla <Dilip.Pulla@paysafe.com>
2021-04-25 20:00:36 +0530

    Build spring boot project

---
+++ src/test/java/com/maha/checkout/MahaCheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maha.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahaCheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
